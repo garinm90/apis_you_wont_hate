b07eca92fc7d57323e17d244b04fb38d12d4be4b
Max Garin <maxgarin@gmail.com>
2024-11-19 21:38:13 -0600

    Add command for initial database creation

--- app/commands.py
+++ app/commands.py
@@ -1,5 +1,6 @@
 import typer
 
+from app.database import create_db_and_tables
 from app.factories import UserFactory
 
 app = typer.Typer(no_args_is_help=True)
@@ -8,6 +9,10 @@ app = typer.Typer(no_args_is_help=True)
 @app.command(help='Generates database fixtures')
 def seed_database():
     UserFactory.create_batch(50)
+
+@app.command(help='Create database and tables if it does not already exists')
+def create_database():
+    create_db_and_tables()
 
 
 @app.callback()
